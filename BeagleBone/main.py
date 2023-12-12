a0feb3f588d283b2587665e6a6cfbe1d3a2850cb
Adam Czyżak <101229929+VadamC27@users.noreply.github.com>
2023-12-12 17:21:35 +0100

    Add exemplary site with "Hello World"

--- BeagleBone/main.py
+++ BeagleBone/main.py
@@ -1,1 +1,17 @@
 
+#!/usr/bin/env python
+
+from flask import flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    templateData = {
+        'title' : 'Hello world' #wszystkie rzeczy przekazywane do html
+        'przyklad' : 'Testowy tekst'
+    }
+    return render_template('site.html', **templateData)
+if __name__ == '__main__':
+    app.run(debug=True, port=8080, host='0.0.0.0')
