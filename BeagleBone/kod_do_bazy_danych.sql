a862acd0aa26b2c7525a11183ef3dd52599bbe73
Adam Czyżak <101229929+VadamC27@users.noreply.github.com>
2023-12-13 13:05:04 +0100

    Add SQL code

---
+++ BeagleBone/kod_do_bazy_danych.sql
@@ -0,0 +1,20 @@
+CREATE TABLE uzytkownicy (login TEXT PRIMARY KEY NOT NULL, 
+                        haslo TEXT NOT NULL);
+
+CREATE TABLE kody(login TEXT NOT NULL,
+                  kod TEXT PRIMARY KEY NOT NULL,
+                  typ TEXT,
+                  FOREIGN KEY (login) REFERENCES uzytkownicy(login));
+
+CREATE TABLE czujniki(id_czujnika INTEGER PRIMARY KEY NOT NULL,
+                      nazwa TEXT NULL);       
+
+CREATE TABLE obserwowane_czujniki(login TEXT NOT NULL,
+                                  id_czujnika INTEGER NOT NULL,
+                                  FOREIGN KEY (login) REFERENCES uzytkownicy(login),
+                                  FOREIGN KEY (id_czujnika) REFERENCES czujniki(id_czujnika));   
+
+CREATE TABLE zarejestrowan_ruch (data TEXT NOT NULL, 
+                                id_czujnika TEXT NOT NULL,
+                                FOREIGN KEY (id_czujnika) REFERENCES czujniki(id_czujnika)));
+
--- BeagleBone/main.py
+++ BeagleBone/main.py
@@ -1,17 +1,37 @@
 
 #!/usr/bin/env python
 
-from flask import flask
+from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
+conn = sqlite3.connect('sam.db')
+c = conn.cursor()
+
+def executeScriptsFromFile(filename):
+    fd = open(filename, 'r')
+    sqlFile = fd.read()
+    fd.close()
+
+    sqlCommands = sqlFile.split(';')
+
+    for command in sqlCommands:
+        try:
+            c.execute(command)
+        except OperationalError, msg:
+            print("Command skipped: ", msg)
+
 @app.route('/')
 def index():
-
     templateData = {
-        'title' : 'Hello world' #wszystkie rzeczy przekazywane do html
-        'przyklad' : 'Testowy tekst'
+        'title' : 'Hello world', #wszystkie rzeczy przekazywane do html
+        'przyklad' : 'Testowy tekst',
     }
+
     return render_template('site.html', **templateData)
+
+
 if __name__ == '__main__':
+    executeScriptsFromFile("kod_do_bazy_danych.sql")
     app.run(debug=True, port=8080, host='0.0.0.0')
