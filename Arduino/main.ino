2b7c9bdf55c96c651dddcf739753de81b82ab25a
Adam Czyżak <101229929+VadamC27@users.noreply.github.com>
2023-12-20 09:33:54 +0100

    Add sensor and keypad

--- Arduino/main.ino
+++ Arduino/main.ino
@@ -1,45 +1,60 @@
 #include <Key.h>
 #include <Keypad.h>
 
-#define Pin
+#define Pin 2
 
 const byte ROWS = 4;
 const byte COLS = 4; 
 
-byte rowPins[ROWS] = {7, 6, 5, 4}; //piny wierszy
+byte rowPins[ROWS] = {4, 5, 6, 7}; //piny wierszy
 byte colPins[COLS] = {8, 9, 10, 11};
 
 char keys[ROWS][COLS] = { //keypad map
-  {'*','0','#','D'},
-  {'7','8','9','C'},
+  {'1','2','3','A'},
   {'4','5','6','B'},
-  {'1','2','3','A'}
+  {'7','8','9','C'},
+  {'*','0','#','D'}
 };
 
 char typedByUser[5] = {'\0', '\0', '\0', '\0', '\0'};
 int length = 0;
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); // init keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins,  ROWS, COLS ); // init keypad
 
 int isTriggered = 0; 
+int armed = 1;
 
+boolean lockLow = true;
+boolean takeLowTime;
+long unsigned int lowIn;         
+long unsigned int pause = 5000;  
+
+void clearPass(){
+  for(int i = 0; i<5;i++){
+    typedByUser[i]='\0';
+  }
+}
 
 void triggerAlarm(int pin){
-  switch(pin){
-    case 2:{
-
-      brake;
-    }
-    case 3:{
-
-      brake;
-    }
-    default:{
-
-      brake;
+    if(armed){
+      isTriggered = 1; 
+      switch(pin){
+      case 2:{      
+        Serial.println("alrm2");
+        break;
+      }
+      case 3:{
+        Serial.println("alrm3");
+        break;
+      }
+      default:{
+        Serial.println("errn1");
+        break;
+      }
     }
   }
 }
+
 
 void detectionPIN2(){
   triggerAlarm(2);
@@ -53,39 +68,47 @@ void chceckKeypad(){
   char key = keypad.getKey();
   if(key == '#'){
     length = 0;
+    clearPass();
   }else if(key == '*'){
-    Serial.print("code ");
+    Serial.print("code");
     Serial.println(typedByUser);
 
-    String input = Serial.readStringUntil('\n');
-    if(input == 0) return; //niesprawdził się
+    /*String input = Serial.readStringUntil('\n');
+    if(input == 0) return; //nie sprawdził się
     if(input == 1) { //poprawny kod zmiana stanu?!
       
-    }
-  }else{
+    }*/
+  }else if(key){
     typedByUser[length] = key;
     length = length + 1;
-    if(length => 4 ) {lenght = 0; typedByUser[5] = {'\0', '\0', '\0', '\0', '\0'};}
+    if(length >= 4 ) {
+      length = 0; 
+      clearPass();
+    }
   }
 }
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("This is setup code");
   //TODO output dla diody
-  pinMode(2, INPUT_PULLUP); 
-  pinMode(3, INPUT_PULLUP); 
-  attachInterrupt(digitalPinToInterrupt(2), detectionPIN2, FALLING); 
-  attachInterrupt(digitalPinToInterrupt(3), detectionPIN3, FALLING); 
+  pinMode(2, INPUT); 
+  pinMode(3, INPUT); 
+  attachInterrupt(digitalPinToInterrupt(2), detectionPIN2, RISING); 
+  attachInterrupt(digitalPinToInterrupt(3), detectionPIN3, RISING); 
+
 }
 
 void loop() {
   chceckKeypad();
+ /* char key = keypad.getKey();
   
-  if(isTriggered){
+  if (key){
+    Serial.println(key);
+  }*/
+  /*if(isTriggered){
     
   } else{
     //TODO dioda ma byc zgaszona 
-  }
-
+  }*/
+ 
 }
