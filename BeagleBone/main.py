ce4ae0ba8c619b5b74589675d04f54531b4f3d88
Maria <mysiol007@gmail.com>
2024-01-09 21:02:13 +0100

    Add templates and flask is working

--- BeagleBone/main.py
+++ BeagleBone/main.py
@@ -3,6 +3,17 @@
 from flask import Flask, render_template, request
 import sqlite3
 from sqlite3 import OperationalError
+#import Adafruit_BBIO.UART as UART
+#import serial
+
+pin = 'P9_26'
+#UART.setup("UART1") # Konfiguracja UART1
+#ser = serial.Serial(port="/dev/ttyS1", baudrate=9600) # Konfiguracja portu szeregowego
+#ser.close()
+#ser.open()
+
+#if ser.isOpen():
+#    kod = ser.readline()
 
 app = Flask(__name__)
 
@@ -39,9 +50,16 @@ def index():
     }
     return render_template('site.html', **templateData)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    templateData = {
+        'title': 'Logowanie',
+        'example': 'Przykładowy tekst dla logowania',
+    }
+    return render_template('login.html', **templateData)
+
 @app.route('/entries', methods=['GET', 'POST'])
 def entries():
-
     if request.method == 'POST':
         if request.form.get('action1') == 'VALUE1':
             print("Buton1")
@@ -56,7 +74,41 @@ def entries():
     }
     return render_template('entries.html', **templateData)
 
+@app.route('/options', methods=['GET', 'POST'])
+def options():
+    templateData = {
+        'title': 'Ustawienia',
+        'example': 'Przykładowy tekst dla ustawień',
+    }
+    return render_template('options.html', **templateData)
+
+@app.route('/change_code', methods=['GET', 'POST'])
+def change_code():
+    templateData = {
+        'title': 'Zmiana kodu',
+        'example': 'Przykładowy tekst dla zmiany kodu',
+    }
+    return render_template('password.html', **templateData)
+
+@app.route('/notification_settings', methods=['GET', 'POST'])
+def notification_settings():
+    templateData = {
+        'title': 'Zmiana kodu',
+        'example': 'Przykładowy tekst dla zmiany kodu',
+    }
+    return render_template('notifications.html', **templateData)
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    templateData = {
+        'title': 'Zmiana kodu',
+        'example': 'Przykładowy tekst dla zmiany kodu',
+    }
+    return render_template('adduser.html', **templateData)
 
 if __name__ == '__main__':
     executeScriptsFromFile("kod_do_bazy_danych.sql")
     app.run(debug=True, port=8080, host='0.0.0.0')
+
+
+#ser.close() # Zamknięcie połączenia szeregowego
